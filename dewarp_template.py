d8b9ada2ec2bf693626e744f387cebd2651539ea
chelicerae <DenMiller45@gmail.com>
2019-07-24 17:45:54 +0200

    Code refactored

--- dewarp_template.py
+++ dewarp_template.py
@@ -15,14 +15,29 @@ def un_sterad():
 
 
 def un_log(yx, params):
+	"""
+	Logarithm model distortion function.
+	yx : np.array
+		(y, x) pixel coordinate.
+	params : {'lambda': float, 's': float}
+		Distortion paramethers dict.
+	"""
+
 	ynd, xnd = yx[0], yx[1]
 	alpha, rd = cartesian_to_polar(ynd, xnd)
 	ru = params['s'] * log(1 + params['lambda']*rd)
 	return polar_to_cartesian(alpha, ru)
 
 
-# A field of view reverce model function. 
 def un_fov(yx, fov):
+	"""
+	A field of view reverce model function. 
+	yx : np.array
+		(y, x) pixel coordinate.
+	fov : float
+		Field of view angle in radians.
+	"""
+
 	ynd, xnd = yx[0], yx[1]
 	alpha, rd = cartesian_to_polar(ynd, xnd)
 	ru = atan(rd*tan(fov))/tan(fov)
@@ -30,39 +45,85 @@ def un_fov(yx, fov):
 
 
 def un_fitzgibbon(yx, params):
+	"""
+	Fitzgibbon distortion model function.
+	yx : np.array
+		(y, x) pixel coordinate.
+	params : {'h': int, 'w': int, 'k': float}
+		Distortion parameters. 'h' and 'w': width and height of the image respectivly.
+		'k': distortion parameter.
+	"""
+
+	# Normaize to (-1, 1) range.
 	ynd, xnd = yx[0]/(params['h']/2), yx[1]/(params['w']/2)
+
 	alpha, rd = cartesian_to_polar(ynd, xnd)
 	ru = rd/(1. - params['k']*(rd**2))
 	ynu, xnu = polar_to_cartesian(alpha, ru)
+
 	# Denormalize from (-1, 1) range.
 	return (ynu*(params['h']/2), xnu*(params['w']/2))
 
 
 # params format {'degree' : degree of polynome, 'ks' : vector of dist coefs equal to degree in length}
 def un_radial(yx, params):
+	"""
+	Radial distortion model function.
+	yx : np.array
+		(y, x) pixel coordinate.
+	params : {'h': int, 'w': int, 'k': float}
+		Distortion parameters. 'h' and 'w': width and height of the image respectivly.
+		'k': distortion parameter.
+	"""
+
 	ynd, xnd = yx[0], yx[1]
 	alpha, rd = cartesian_to_polar(ynd, xnd)
+
 	# Count r pawer series and dot product of k's and r power series.
 	dgr = params['degree']
 	powers = np.array(range(dgr))
 	rd_vec = np.power(np.array([rd]*dgr), powers)
 	ru = rd * np.dot(params['ks'].T, rd_vec)
+
 	return polar_to_cartesian(alpha, ru)
 
 
 # params format {'degree' : degree of polynome, 'ks' : vector of dist coefs equal to degree in length}
-def un_radial(yx, params):
+def un_division(yx, params):
+	"""
+	Division distortion model function.
+	yx : np.array
+		(y, x) pixel coordinate.
+	params : {'h': int, 'w': int, 'k': float}
+		Distortion parameters. 'h' and 'w': width and height of the image respectivly.
+		'k': distortion parameter.
+	"""
+
 	ynd, xnd = yx[0], yx[1]
 	alpha, rd = cartesian_to_polar(ynd, xnd)
+
 	# Count r pawer series and dot product of k's and r power series.
 	dgr = params['degree']
 	powers = np.array(range(dgr))
 	rd_vec = np.power(np.array([rd]*dgr), powers)
-	ru = rd * np.dot(params['ks'].T, rd_vec)
+	ru = rd / np.dot(params['ks'].T, rd_vec)
+
 	return polar_to_cartesian(alpha, ru)
 
 
 def dist_remap(h, w, py=0, px=0, dist_func, dist_params):
+	"""
+	Returns two remap function for x and y respectively using dist_func to indexes.
+	h, w : int, int
+		Heinghts and width of the image.
+	py, px : int, int
+		h and w padding for displaying bigger part og an image in case it is strached beyond original size.
+	dist_func : function
+		Distortion model function that creates the mapping from one image to another.
+	dist_params : any
+		Model specific distortion parameters. 
+	"""
+
 	# Create indeces remap matrix. 
 	mapxy = np.indices((h+2*py, w+2*px))
 
@@ -80,6 +141,20 @@ def dist_remap(h, w, py=0, px=0, dist_func, dist_params):
 
 
 def dewarp_video(video_dir, save_dir, dist_func, dist_params, py=0, px=0):
+	"""
+	Calculates remapping matrix and displays the video dewarping it in real-time.
+	video_dir : string
+		Directory of video
+	save_dir : string
+		Save directory of dewarped video.
+	dist_func : function
+		Distortion model function that creates the mapping from one image to another.
+	dist_params : any
+		Model specific distortion parameters. 
+	py, px : int, int
+		Images h and w padding for displaying bigger part of an image in case it is strached beyond original size.
+	"""
+
 	cap = cv2.VideoCapture(video_dir)
 	fourcc = cv2.VideoWriter_fourcc(*'XVID')
 	# out = cv2.VideoWriter('output.avi',fourcc, 20.0, (640,480))
@@ -105,6 +180,20 @@ def dewarp_video(video_dir, save_dir, dist_func, dist_params, py=0, px=0):
 
 
 def dist_img(src_dir, dist_func, dist_params, py=0, px=0):
+	"""
+	Calculates remapping matrix and uses it to the given image.
+	src_dir : string
+		Directory of image
+	save_dir : string
+		Save directory of dewarped image.
+	dist_func : function
+		Distortion model function that creates the mapping from one image to another.
+	dist_params : any
+		Model specific distortion parameters. 
+	py, px : int, int
+		Images h and w padding for displaying bigger part of an image in case it is strached beyond original size.
+	"""
+
 	# Resize image window for consistancy.
 	cv2.namedWindow('fisheye', cv2.WINDOW_NORMAL)
 	cv2.resizeWindow('fisheye', 900, 600)
